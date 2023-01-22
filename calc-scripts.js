0bc82deaa9dba6d9c093bcc7ee357a283859b588
Phil Taylor <phil.c.taylor@gmail.com>
2023-01-21 23:21:22 -0500

    first draft. calc cut-out with display functionality.

---
+++ calc-scripts.js
@@ -0,0 +1,14 @@
+const ans = document.getElementById("answer");
+const btnDivs = document.getElementsByClassName("btn");
+let currentDiv = null;
+for (var i = 0; i < btnDivs.length; i++) {
+    btnDivs[i].addEventListener("click", function() {
+        if (currentDiv) {
+            currentDiv.style.backgroundColor = "";
+        }
+        this.style.backgroundColor = "rgb(0, 0, 0, 0.5)";
+        currentDiv = this;
+        const userInput = this.textContent;
+        ans.textContent = userInput;
+    });
+}
